ed4e956cebf2cea9195a0a54217c88f51d84ed68
Juan Sebastian Perniola <jperniola@nigul.cide.es>
2024-01-12 14:04:21 +0100

    Actualizacion del 12/01

--- Actividad7/index.js
+++ Actividad7/index.js
@@ -1,9 +1,13 @@
 _toggle.onclick = () => {
+    //manejador de cuando aparece las clases .open y .close
     _items.classList.toggle("open");
     _items.classList.toggle("close");
-    const links = document.querySelectorAll("#_items a");
 
-    // Agregar un manejador de eventos a cada enlace
+    //creo variable que guarda los links
+    const links = document.querySelectorAll("a");
+
+    //  agrego un manejador de eventos para que cada vez que le demos a un link se cierre
+    //Preguntar al profesor cuan de correcto es esto 
     links.forEach(link => {
         link.addEventListener("click", () => {
             // Cerrar el menú al hacer clic en un enlace
