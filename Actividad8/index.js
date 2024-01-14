04c2ffeb730f2e93e4d702b26ec69f03247f4f7e
Juan Sebastian Perniola <jperniola@nigul.cide.es>
2024-01-14 11:18:06 +0100

    Actividad 8

--- Actividad8/index.js
+++ Actividad8/index.js
@@ -1,43 +1,56 @@
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
+//cruz par a borrar
+document.querySelectorAll('li').forEach(function (element) {
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+  element.appendChild(span);
 
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
+  span.onclick = function () {
+    removeTask(this);
+  };
+});
+
+function removeTask(element) {
+  var listItem = element.parentElement;
+  listItem.style.display = "none";
 }
+//elimnamos elementos
+document.querySelectorAll('.close').forEach(function (closeButton) {
+  closeButton.onclick = function () {
+    var listItem = closeButton.parentElement;
+    listItem.style.display = "none";
+  };
+});
 
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
+list.addEventListener('click', function (ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
   }
-}, false);
-
-// Create a new list item when clicking on the "Add" button
+}
+);
+ 
+//doc.elemtn = que hace que cuando pulsemos "Enter" se añdaa una tarea 
+ document.getElementById("myInput").addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    newElement();
+  }
+});
+// Crear nueva tarea por hacer 
 function newElement() {
   var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
+  var inputValue = document.getElementById("myInput").value; //.value pilla el valor de texto introducido
   var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
+
+  if (inputValue === '') { // si al pulsar add, no hay nada, salta un aler
+    alert("Oops, parece que no has escrito nada, vuelve a intentarlo!");
+    return; // Sale de la función si no hay valor
   }
+
+  li.appendChild(t);
+  document.getElementById("myUL").appendChild(li);
   document.getElementById("myInput").value = "";
 
   var span = document.createElement("SPAN");
@@ -46,24 +59,32 @@ function newElement() {
   span.appendChild(txt);
   li.appendChild(span);
 
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
+  // Asigna el evento onclick después de crear el nuevo elemento
+  span.onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+
+  // Reasigna los eventos de clic para los elementos close existentes
+  for (var i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
       var div = this.parentElement;
       div.style.display = "none";
-    }
+    };
   }
 }
 
+
 var showAllTasks = false; // Variable para controlar si se deben mostrar todas las tareas o solo las completadas
 
-// Function to filter tasks
+// Funcion filtrar listas hechas y cuales no 
 function filterTasks() {
   var list = document.getElementById("myUL");
   var items = list.getElementsByTagName("li");
 
   for (var i = 0; i < items.length; i++) {
     var item = items[i];
-    
+
     if (showAllTasks || item.classList.contains("checked")) {
       item.style.display = "list-item";
     } else {
@@ -73,4 +94,34 @@ function filterTasks() {
 
   // Alternar entre mostrar todas las tareas y solo las completadas
   showAllTasks = !showAllTasks;
+}
+
+
+function filterTasks(filterType) {
+  var list = document.getElementById("myUL");
+  var items = list.getElementsByTagName("li");
+
+  for (var i = 0; i < items.length; i++) {
+      var item = items[i];
+
+      switch (filterType) {
+          case 'all':
+              item.style.display = "list-item";
+              break;
+          case 'completed':
+              if (item.classList.contains("checked")) {
+                  item.style.display = "list-item";
+              } else {
+                  item.style.display = "none";
+              }
+              break;
+          case 'checked':
+              if (item.classList.contains("checked")) {
+                  item.style.display = "none";
+              } else {
+                  item.style.display = "list-item";
+              }
+              break;
+      }
+  }
 }
