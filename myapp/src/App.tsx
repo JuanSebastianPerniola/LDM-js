b1d4449d87c135f5f17d42d8031868c85d394c15
Juan Sebastian Perniola <jperniola@nigul.cide.es>
2024-02-06 18:07:14 +0100

    update

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -1,32 +1,93 @@
-// App.js
 import React, { useState } from "react";
 import "./App.css";
-import { Button } from "./Button";
+import Stack from 'react-bootstrap/Stack';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+import classNames from 'classnames'; // Importa la biblioteca classNames para manejar múltiples clases CSS
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
 
 export default function App() {
-  const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState('');
+  const [tasks, setTasks] = useState<string[]>([]);
+  const [newTask, setNewTask] = useState<string>('');
+  const [listColor, setNewColor] = useState<string>('');
+  const [completedTasks, setCompletedTasks] = useState<boolean[]>([]); // Estado para rastrear las tareas completadas
 
   const handleAddTask = () => {
-    if (newTask.trim() !== '') {
+    if(newTask === " "){{[
+      'primary',
+      'secondary',
+      'success',
+      'danger',
+      'warning',
+      'info',
+      'light',
+      'dark',
+    ].map((variant) => (
+      <Alert key={variant} variant={variant}>
+         Fist you got to add some text down there lad{variant}
+       
+      </Alert>
+    ))}
+     
+    }
+    else if (newTask.trim() !== '') {
       setTasks([...tasks, newTask]);
+      setCompletedTasks([...completedTasks, false]); // Inicialmente, ninguna tarea está completada
       setNewTask('');
     }
   };
 
-  const handleDeleteTask = () => {
-    // Lógica para eliminar tareas (si es necesario)
+  const handleDeleteTask = (index: number) => {
+    const updatedTasks = [...tasks];
+    //aray con tareas 
+    updatedTasks.splice(index, 1);
+    //eliminamos (splice).arrays 
+    setTasks(updatedTasks);
+    const updatedCompletedTasks = [...completedTasks];
+    updatedCompletedTasks.splice(index, 1);
+    setCompletedTasks(updatedCompletedTasks);
+  };
+
+  const handleMarked = (index: number) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index] = '✔️ ' + updatedTasks[index]; // agregamos un check a la izquierda de la lista 
+    setTasks(updatedTasks);
+    setNewColor('gray'); //Cambiamos el color del contenid de la lista a gris 
+    const updatedCompletedTasks = [...completedTasks];
+    updatedCompletedTasks[index] = true; // seteo como true cada vez que creo una lista 
+    setCompletedTasks(updatedCompletedTasks);
   };
 
   return (
-    <div className='App'>
-      <h1>Todo List</h1>
-      <input
-        type="text"
-        value={newTask}
-        onChange={(e) => setNewTask(e.target.value)}
-      />
-      <Button onAddTask={handleAddTask} onDeleteTask={handleDeleteTask} />
-    </div>
+    <body>
+      <div className='App'>
+        <h1>Todo List</h1>
+        <FloatingLabel controlId="floatingInput" label="Add your item here...">
+          <Form.Control 
+            type="text" 
+            placeholder="Add your item here..." 
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+          />
+        </FloatingLabel>
+        <Button variant="primary" onClick={() => handleAddTask()}>Add task</Button>{' '}
+        {/* <button >Add Task</button> */}
+        <Stack>
+          {tasks.map((task, index) => (
+            <li key={index} className={classNames({ 'completed-task': completedTasks[index] })}>
+              {task}
+              <Button onClick={() => handleDeleteTask(index)}>delete</Button>
+              <Button 
+                onClick={() => handleMarked(index)} 
+                disabled={completedTasks[index]} // Deshabilita el botón si la tarea está completada
+              >
+                Mark as Done
+              </Button>
+            </li>
+          ))}
+        </Stack>
+      </div>
+    </body>
   );
 }
--- myapp/src/index.tsx
+++ myapp/src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 const root = ReactDOM.createRoot(
@@ -15,8 +16,8 @@ root.render(
   </React.StrictMode>
 );
 
-let a = "hola caracola" 
-console.log(a)
+// let a = "hola caracola" 
+// console.log(a)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
